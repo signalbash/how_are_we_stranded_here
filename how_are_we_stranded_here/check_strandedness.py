b5ca77b13e251b11dcbfc3a79c5b7798908c5b36
aleighbrown <brown.annaleigh@gmail.com>
2019-11-28 13:56:51 +0000

    Forgot a plus

--- how_are_we_stranded_here/check_strandedness.py
+++ how_are_we_stranded_here/check_strandedness.py
@@ -120,7 +120,7 @@ def main():
     if(is_gz_file(reads_1)):
         cmd = 'zcat < ' + reads_1 + ' | head -n ' + str(n_reads * 4) + ' > ' + reads_1_sample
     else:
-        cmd = 'head ' reads_1 + ' -n ' + str(n_reads * 4) + ' > ' + reads_1_sample
+        cmd = 'head ' + reads_1 + ' -n ' + str(n_reads * 4) + ' > ' + reads_1_sample
     if print_cmds:
         print('running command: ' + cmd)
     subprocess.call(cmd, shell=True)
@@ -130,7 +130,7 @@ def main():
     if(is_gz_file(reads_2)):
         cmd = 'zcat < ' + reads_2 + ' | head -n ' + str(n_reads * 4) + ' > ' + reads_2_sample
     else:
-        cmd = 'head ' reads_2 + ' -n ' + str(n_reads * 4) + ' > ' + reads_1_sample
+        cmd = 'head ' + reads_2 + ' -n ' + str(n_reads * 4) + ' > ' + reads_1_sample
     subprocess.call(cmd, shell=True)
 
     # align with kallisto
